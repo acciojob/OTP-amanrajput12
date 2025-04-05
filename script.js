8ca2ec5a7cd215f3cfaf9d1120889d6c66ef8372
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-04-05 14:07:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code")
+
+inputs.forEach((data,i)=>{
+    data.addEventListener("input",(e)=>{
+        e.preventDefault()
+        const value =e.target.value;
+        if(value){
+            if(i<inputs.length-1){
+                inputs[i+1].focus();
+            }
+        }
+    });
+
+    data.addEventListener("keydown",(e)=>{
+        if(e.key=='Backspace'){
+            e.preventDefault()
+        
+            // console.log("keydown click",i)
+           if(i>=0){
+            e.target.value=""
+            inputs[i-1].focus()
+           }
+        }
+    })
+})
+
+
